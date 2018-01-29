764905622d4ec6d1eefc40a7825f28e025eeaa73
Cesar Gutierrez <cesar@tineo.mobi>
2018-01-29 15:08:18 -0500

    initial d

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tineo
+ * Date: 29/01/18
+ * Time: 02:22 AM
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends Controller
+{
+    /**
+     * @Route("/", name="searh_page")
+     */
+    public function index()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('search/index.html.twig', array('number' => $number));
+    }
+}
